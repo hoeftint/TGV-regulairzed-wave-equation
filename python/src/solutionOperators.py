2aafdd600fe47b2addb28b58b4320104b15d9c06
Thomas <thomas.hoefting@gmx.de>
2024-12-02 10:56:02 +0100

    Made solution method selectable

--- python/src/solutionOperators.py
+++ python/src/solutionOperators.py
@@ -67,8 +67,19 @@ def buildControlFunctionAdjoint(sources, control, params):
 			signals[idx,j] = energy_global
 	return signals
 
-'''
 def solveStateEquation(control: List[fem.Function], params) -> List[fem.Function]:
+    if params.fdType == 'backward-euler':
+        return solveStateEquationBackwards(control, params)
+    elif params.fdType == 'mid-rule':
+        return solveStateEquationBackwards(control, params)
+    raise Exception('Unknown finite difference solver')
+
+def solveAdjointEquation(control: List[fem.Function], params) -> List[fem.Function]:
+	control_rev = []
+	control_rev[:] = list(reversed(control))
+	return list(reversed(solveStateEquation(control_rev, params)))[:]
+
+def solveStateEquationBackwards(control: List[fem.Function], params) -> List[fem.Function]:
 	uStart = fem.Function(params.V)
 	uStart.interpolate(lambda x : np.zeros(x[0].shape))
 	solution = [uStart]
@@ -106,41 +117,7 @@ def solveStateEquation(control: List[fem.Function], params) -> List[fem.Function
 		u2.x.array[:] = u.x.array
 	return solution
 
-def solveAdjointEquation(control: List[fem.Function], params, bcs=[]):
-	pStart = fem.Function(params.V)
-	pStart.interpolate(lambda x : np.zeros(x[0].shape))
-	solution = [pStart]
-	pT = fem.Function(params.V)
-	pT.interpolate(lambda x : np.zeros(x[0].shape))
-	pT1 = fem.Function(params.V)
-	pT1.interpolate(lambda x : np.zeros(x[0].shape))
-	pT2 = fem.Function(params.V)
-	pT2.interpolate(lambda x : np.zeros(x[0].shape))
-	p = ufl.TrialFunction(params.V)
-	v = ufl.TestFunction(params.V)
-	g = fem.Function(params.V)
-	g.x.array[:] = control[0].x.array
- 
-	# diffusion
-	#a = 3 * inner(p,  v) * dx + 2 * (params.dt * params.waveSpeed**2) * inner(grad(p),grad(v)) * dx
-	#L = 4 * inner(pT, v)*dx - inner(pT1, v)*dx + 2 * params.dt * inner(g, v) * dx
-
-	# wave
-	a =  2 * inner(p,  v) * dx + (params.dt**2 * params.waveSpeed**2) * inner(grad(p),grad(v)) * dx
-	L = 5 * inner(pT, v)*dx - 4 * inner(pT1, v) * dx + inner(pT2, v)*dx + params.dt**2 * inner(g, v) * dx
-	biggestIdx = int(params.T/params.dt)
-	for idx in range(biggestIdx):
-		g.x.array[:] = control[biggestIdx - idx].x.array
-		problem = LinearProblem(a, L, bcs=bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
-		p = problem.solve()
-		solution.append(p)
-		pT2.x.array[:] = pT1.x.array
-		pT1.x.array[:] = pT.x.array
-		pT.x.array[:] = p.x.array
-	solution.reverse()
-	return solution
-'''
-def solveStateEquation(control: List[fem.Function], params) -> List[fem.Function]:
+def solveStateEquationMid(control: List[fem.Function], params) -> List[fem.Function]:
 	solution = []
 	# Set up time integration scheme:
 	u = fem.Function(params.V)
@@ -182,7 +159,38 @@ def solveStateEquation(control: List[fem.Function], params) -> List[fem.Function
 	solution.append(temp)
 	return solution
 
-def solveAdjointEquation(control: List[fem.Function], params) -> List[fem.Function]:
-	control_rev = []
-	control_rev[:] = list(reversed(control))
-	return list(reversed(solveStateEquation(control_rev, params)))[:-1]
+'''
+def solveAdjointEquation(control: List[fem.Function], params, bcs=[]):
+	pStart = fem.Function(params.V)
+	pStart.interpolate(lambda x : np.zeros(x[0].shape))
+	solution = [pStart]
+	pT = fem.Function(params.V)
+	pT.interpolate(lambda x : np.zeros(x[0].shape))
+	pT1 = fem.Function(params.V)
+	pT1.interpolate(lambda x : np.zeros(x[0].shape))
+	pT2 = fem.Function(params.V)
+	pT2.interpolate(lambda x : np.zeros(x[0].shape))
+	p = ufl.TrialFunction(params.V)
+	v = ufl.TestFunction(params.V)
+	g = fem.Function(params.V)
+	g.x.array[:] = control[0].x.array
+ 
+	# diffusion
+	#a = 3 * inner(p,  v) * dx + 2 * (params.dt * params.waveSpeed**2) * inner(grad(p),grad(v)) * dx
+	#L = 4 * inner(pT, v)*dx - inner(pT1, v)*dx + 2 * params.dt * inner(g, v) * dx
+
+	# wave
+	a =  2 * inner(p,  v) * dx + (params.dt**2 * params.waveSpeed**2) * inner(grad(p),grad(v)) * dx
+	L = 5 * inner(pT, v)*dx - 4 * inner(pT1, v) * dx + inner(pT2, v)*dx + params.dt**2 * inner(g, v) * dx
+	biggestIdx = int(params.T/params.dt)
+	for idx in range(biggestIdx):
+		g.x.array[:] = control[biggestIdx - idx].x.array
+		problem = LinearProblem(a, L, bcs=bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+		p = problem.solve()
+		solution.append(p)
+		pT2.x.array[:] = pT1.x.array
+		pT1.x.array[:] = pT.x.array
+		pT.x.array[:] = p.x.array
+	solution.reverse()
+	return solution
+'''
